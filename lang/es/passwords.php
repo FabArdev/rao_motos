39982f4f0729444be60c618c660808a6cdeca425
ReyMar81 <loaizalabardenreymar@gmail.com>
2026-07-01 11:52:13 -0400

    cambios minimos

---
+++ lang/es/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'Su contraseña ha sido restablecida.',
+    'sent' => 'Le hemos enviado el enlace para restablecer su contraseña por correo.',
+    'throttled' => 'Por favor espere antes de reintentar.',
+    'token' => 'El token de restablecimiento de contraseña es inválido.',
+    'user' => 'No encontramos un usuario con esa dirección de correo.',
+
+];
